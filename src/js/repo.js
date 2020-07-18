f70223c7206ca42c1171c3c0d8960e9caa43bd66
kiselevgleb <kiselevgleb@gmail.com>
2020-07-18 13:43:15 +0300

    change server url on the heroku

--- src/js/repo.js
+++ src/js/repo.js
@@ -7,7 +7,7 @@ export {
 
 function allTickets() {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://kiselevgleb.github.io/HelpDesk-backEnd/?method=allTickets', false);
+  xhr.open('GET', 'https://help-desk-backend2.herokuapp.com/?method=allTickets', false);
   xhr.send();
   if (xhr.status !== 200) {
     alert(`${xhr.status}: ${xhr.statusText}`);
@@ -16,7 +16,7 @@ function allTickets() {
   }
 }
 function ticketById(id) {
-  const url = `https://kiselevgleb.github.io/HelpDesk-backEnd/?method=ticketById&id=${id}`;
+  const url = `https://help-desk-backend2.herokuapp.com/?method=ticketById&id=${id}`;
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url, false);
   xhr.send();
@@ -27,7 +27,7 @@ function ticketById(id) {
   }
 }
 function delById(form) {
-  const url = 'https://kiselevgleb.github.io/HelpDesk-backEnd/?method=delById';
+  const url = 'https://help-desk-backend2.herokuapp.com/?method=delById';
   const xhr = new XMLHttpRequest();
   xhr.open('POST', url, false);
   xhr.send(form);
@@ -38,7 +38,7 @@ function delById(form) {
   }
 }
 function createTicket(formData) {
-  const url = 'https://kiselevgleb.github.io/HelpDesk-backEnd/?method=createTicket';
+  const url = 'https://help-desk-backend2.herokuapp.com/?method=createTicket';
   const xhr = new XMLHttpRequest();
   xhr.open('POST', url, false);
   xhr.send(formData);
